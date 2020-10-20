fec084b817a4511b190492b49ba7bce5276410a9
Gabriel Da Silva <gabriel.silva@FMI.local>
2020-10-20 16:47:22 -0300

    feat: created user not found exception

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,21 +26,26 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
 		
 		URI location = ServletUriComponentsBuilder
 			.fromCurrentRequest()
 			.path("/{id}")
 			.buildAndExpand(savedUser.getId()).toUri();
-
-		ResponseEntity.created(location).build();
 		
+		return ResponseEntity.created(location).build();	
 	}
 	
 	@GetMapping("users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		if(user==null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+		
+		return user;
 	}
 
 }
