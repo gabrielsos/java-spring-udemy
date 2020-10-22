2057a8ad478d5e61293b780f825b9184eb868655
Gabriel Silva <gabrielsos99@gmail.com>
2020-10-22 14:06:42 -0300

    added users posts route

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,7 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -9,8 +14,11 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description="All details about the user.")
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message="O nome precisa ter pelo menos 2 caracteres")
@@ -21,6 +29,9 @@ public class User {
 	@ApiModelProperty(notes="Birth date should be in the past")
 	private Date birthDate;
 	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts; 
+	
 	protected User() {
 		
 	}
@@ -30,6 +41,14 @@ public class User {
 		this.id = id;
 		this.name = name;
 		this.birthDate = birthDate;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	public Integer getId() {
